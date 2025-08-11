d960b2fb3dc4edf34efc3cf124d1df02703dc5ba
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-08-11 21:03:01 +0600

    Basic Server Setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) =>{
+    res.send("The Auth Server Running...");
+})
+
+app.listen(port, () =>{
+    console.log(`The server is running ${port} port`);
+})
